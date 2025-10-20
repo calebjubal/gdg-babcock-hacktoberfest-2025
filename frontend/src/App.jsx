8b9a7f9628ab85603f66b9767cbb531caa4e6d4d
Chetan Chaudhari <chetankc2005@gmail.com>
2025-10-20 18:00:14 +0530

    Implemented Toast Notification in CertificateForm

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import Footer from './components/Footer/Footer.jsx';
 import NotFound from './pages/NotFound/NotFound.jsx';
 import CertificateForm from './pages/CertificationForm/CertificateForm.jsx';
 import Home from './pages/Home/Home.jsx';
+import { Toaster } from 'react-hot-toast';
 
 function App() {
   return (
@@ -12,6 +13,16 @@ function App() {
       <div className="app-container">
         <Navbar />
         <main className="app-main">
+          <Toaster
+            position="top-right"
+            gutter={16}
+            toastOptions={{
+              duration: 5000,
+              success: { duration: 5000 },
+              error: { duration: 6000 },
+              loading: { duration: Infinity },
+            }}
+          />
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/certificate" element={<CertificateForm />} />
